dc8c64edb5431c925c46944cfb511ebc1df7d598
Dexyod <dexyod43@gmail.com>
2020-07-20 18:28:34 +0000

    added tail.js

---
+++ tail.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const tail = (array) => {
+  let tailedArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailedArray.push(array[i]);
+  }
+  return tailedArray;
+};
+
+// TEST CODE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+
+assertEqual(words.length, 3);
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
