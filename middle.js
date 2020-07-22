1acfeeedb61adc63f05c4766342c4d59e0f0a689
Dexyod <dexyod43@gmail.com>
2020-07-21 20:03:12 -0400

    added middle

--- middle.js
+++ middle.js
@@ -21,12 +21,14 @@ const assertArraysEqual = function (actual, expected) {
 };
 
 //ACTUAL FUNCTION
-const middle = function (array) {};
+const middle = function (array) {
+  return array;
+};
 
 //TEST CODE
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
