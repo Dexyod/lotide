5bfab7bae9a778f988eba340025bfc10aa8f5290
Dexyod <dexyod43@gmail.com>
2020-07-24 16:08:18 -0400

    updated eqArrays with recursion

--- eqArrays.js
+++ eqArrays.js
@@ -8,16 +8,17 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  if (Array.isArray(array1) && Array.isArray(array2)) {
+    if (array1.length !== array2.length) return false;
+    for (let i = 0; i < array1.length; i++) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
     }
+    return true;
+  } else {
+    return array1 === array2;
   }
-  return true;
 };
 
 //TEST CODE
@@ -27,3 +28,17 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  ),
+  false
+); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
