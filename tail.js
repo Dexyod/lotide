dc4714ede1fbdbf1be779923e8634c5cf3c8170d
Dexyod <dexyod43@gmail.com>
2020-07-27 12:26:55 -0400

    added comment to tail

--- tail.js
+++ tail.js
@@ -2,6 +2,7 @@
 const tail = (array) => {
   let tailedArray = [];
   for (let i = 1; i < array.length; i++) {
+    //start at index 1 as we don't want the first element
     tailedArray.push(array[i]);
   }
   return tailedArray;
