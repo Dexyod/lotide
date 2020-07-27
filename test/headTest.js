bdd181b1c6a22a669118390543949afe3c2f5257
Dexyod <dexyod43@gmail.com>
2020-07-27 12:53:54 -0400

    added mocha and chai and implemented tests for headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([7]), 7);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head([7]), 7);
+// assertEqual(head([]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("should return first element name of an array of strings", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
